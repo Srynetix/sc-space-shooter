1752b0e280b8fd15dd67fd4369a365f8bba6ab4d
Denis Bourge <denis.bourge@sharingcloud.com>
2019-05-08 19:19:27 +0200

    feat: go to next screen after instructions

--- screens/title_screen/TitleScreen.gd
+++ screens/title_screen/TitleScreen.gd
@@ -7,38 +7,47 @@ onready var instructions = $Instructions
 onready var animation_player = $AnimationPlayer
 onready var high_score = $VBoxContainer/HSValue
 
+var instructions_loaded = false
+
 ###################
 # Lifecycle methods
 
 func _ready():
     if Utils.is_mobile_platform():
-        instructions.text = "Touch to start"
+        self.instructions.text = "Touch to start"
     else:
-        instructions.text = "Press X to start"
+        self.instructions.text = "Press X to start"
         
     var high_score_entry = GameState.get_high_score()
-    high_score.text = "{name} {score}".format({"name": high_score_entry[0], "score": high_score_entry[1]})
+    self.high_score.text = "{name} {score}".format({"name": high_score_entry[0], "score": high_score_entry[1]})
     
-    animation_player.play("title")
-    yield(animation_player, "animation_finished")
-    animation_player.play("instructions")
+    self.animation_player.play("title")
+    yield(self.animation_player, "animation_finished")
+    self.animation_player.play("instructions")
+    self.instructions_loaded = true
     
 func _process(delta):
+    if !self.instructions_loaded:
+        return
+        
     if Input.is_action_just_pressed("player_shoot"):
-        _load_next()
+        self._load_next()
 
 func _input(event):
+    if !self.instructions_loaded:
+        return
+        
     if event is InputEventScreenTouch:
-        _load_next()
+        self._load_next()
         
 func _notification(what):
     if (what == MainLoop.NOTIFICATION_WM_GO_BACK_REQUEST):
-        get_tree().quit()
+        self.get_tree().quit()
         
 #################
 # Private methods
         
 func _load_next():
-    set_process(false)
-    set_process_input(false)
+    self.set_process(false)
+    self.set_process_input(false)
     GameState.load_screen(GameState.Screens.GAME)
