5db26b5e79f30c6a57e3aefbe41244dfc80f288d
Denis Bourge <denis.bourge@sharingcloud.com>
2020-01-28 18:27:19 +0100

    feat: AA on fxwave

--- tests/scenes/TestFXWave.cs
+++ tests/scenes/TestFXWave.cs
@@ -1,43 +1,23 @@
 using Godot;
 
-using Dictionary = Godot.Collections.Dictionary;
-
 public class TestFXWave : Control {
     private static PackedScene fxWaveScene = (PackedScene)GD.Load("res://objects/FXWave.tscn");
 
-    [BindNode]
-    private Spawner spawner;
-    [BindNode]
-    private Player player;
     [BindNodeRoot]
     private GameState gameState;
 
-    public override void _Ready() {
+    async public override void _Ready() {
         this.BindNodes();
 
-        GD.Randomize();
+        await ToSignal(GetTree().CreateTimer(1.0f), "timeout");
 
-        spawner.ConnectTargetScene(this, new Dictionary {
-            { "fire", nameof(_On_Fire) }
-        });
-        player.Connect("fire", this, nameof(_On_Fire));
-        player.Connect("respawn", this, nameof(_On_Respawn));
-        player.GetBulletSystem().Connect("type_switch", this, nameof(_On_TypeSwitch));
-    }
+        while (true) {
+            var wave = (FXWave)fxWaveScene.Instance();
+            AddChild(wave);
+            wave.Start(gameState.GetGameSize() / 2, 0.35f);
 
-    private void _On_Fire(Bullet.FireData fireData) {
-        var bullet = (Bullet)fireData.bullet.Instance();
-        bullet.Prepare(fireData);
-        AddChild(bullet);
-    }
-
-    private void _On_Respawn() {
-        player.Respawn();
-    }
-
-    private void _On_TypeSwitch(Bullet.BulletType prevType, Bullet.BulletType newType) {
-        if (newType != Bullet.BulletType.Bomb) {
-            player.GetBulletSystem().SwitchType(Bullet.BulletType.Bomb);
+            await ToSignal(wave, "finished");
+            await ToSignal(GetTree().CreateTimer(1.0f), "timeout");
         }
     }
 }
