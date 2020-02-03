a50df81fd537b3ff8afadca286a3237cf74161af
Denis Bourge <denis.bourge@sharingcloud.com>
2020-02-03 08:57:50 +0100

    fix: respawn boss in TestBoss

--- tests/scenes/TestBoss.cs
+++ tests/scenes/TestBoss.cs
@@ -4,31 +4,38 @@ public class TestBoss : Node2D {
 
     [BindNode]
     private Player player;
-    [BindNodeRoot]
-    private GameState gameState;
 
     private static PackedScene bossScene = (PackedScene)GD.Load("res://objects/BossEnemy.tscn");
 
     public override void _Ready() {
         this.BindNodes();
 
-        var gameSize = gameState.GetGameSize();
         player.Connect("fire", this, nameof(_On_Fire));
         player.Connect("respawn", this, nameof(_On_Respawn));
 
-        var bossInstance = (BossEnemy)bossScene.Instance();
-        bossInstance.Connect("fire", this, nameof(_On_Fire));
-        bossInstance.Prepare(new Vector2(gameSize.x / 2, -100), 100, 1.0f);
-        AddChild(bossInstance);
+        _SpawnBoss();
     }
 
     private void _On_Fire(Bullet.FireData fireData) {
-        var instance = (Bullet)fireData.bullet.Instance();
+        var instance = fireData.bullet.InstanceAs<Bullet>();
         instance.Prepare(fireData);
         AddChild(instance);
     }
 
     private void _On_Respawn() {
         player.Respawn();
+    }
+
+    private void _On_Boss_Exploded(Node2D instance) {
+        CallDeferred(nameof(_SpawnBoss));
+    }
+
+    private void _SpawnBoss() {
+        var gameSize = GameState.GetInstance(this).GetGameSize();
+        var bossInstance = bossScene.InstanceAs<BossEnemy>();
+        bossInstance.Connect("fire", this, nameof(_On_Fire));
+        bossInstance.Connect("exploded", this, nameof(_On_Boss_Exploded));
+        bossInstance.Prepare(new Vector2(gameSize.x / 2, -100), 100, 1.0f);
+        AddChild(bossInstance);
     }
 }
